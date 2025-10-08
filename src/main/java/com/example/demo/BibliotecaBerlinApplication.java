2ec0f8361c7676df6e53c0fbb212e0f795daa067
Flavio Zavaleta <115955537+Flannviox@users.noreply.github.com>
2025-10-08 11:40:55 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/BibliotecaBerlinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBerlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBerlinApplication.class, args);
+	}
+
+}
